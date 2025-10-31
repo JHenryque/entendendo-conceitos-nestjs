7b28fabee0022a8b2e328c35e9ede844189254e2
JHenrique <henryque.pc.game@gmail.com>
2025-10-31 16:43:15 -0300

    Conhecendo Pipe

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,19 @@
+import { IsBoolean, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({ message: 'Name must be a string' })
+  @MinLength(5, { message: 'Name must be at least 53 characters long' })
+  @IsNotEmpty()
+  //@MaxLength(50, { message: 'Name must be at most 50 characters long' })
   readonly name: string;
+  @IsString({ message: 'Name must be a string' })
+  @MinLength(5, { message: 'Name must be at least 5 characters long' })
+  @IsNotEmpty()
   readonly cargo: string;
+  @IsString()
+  @MinLength(5)
+  @IsNotEmpty()
   readonly description: string;
+  @IsBoolean()
   readonly isCompleted: boolean;
 }
