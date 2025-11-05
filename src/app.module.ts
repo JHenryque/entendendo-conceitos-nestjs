11f8f73a5f28201929926fbc65ae2b9bc194d26e
JHenrique <henryque.pc.game@gmail.com>
2025-11-05 10:30:37 -0300

    Conhecendo middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [TasksModule, UsersModule],
   controllers: [AppController, TasksController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+    // ou .forRoutes(''users', ou 'tasks');
+  }
+}
---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,25 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    //console.log('Request...');
+
+    const authorization = req.headers.authorization;
+
+    if (authorization) {
+      req['user'] = {
+        token: authorization,
+        name: 'Teste',
+      };
+      if (authorization === '123456') {
+        console.log('Token válido');
+        return next();
+      }
+      res.status(401).json({ message: 'Token inválido' });
+    }
+
+    next();
+  }
+}
